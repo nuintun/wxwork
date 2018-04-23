8bb3e8b0d3185f44f6efd3888cdfe27a4e8072e8
nuintun <nuintun@qq.com>
2018-04-23 14:34:52 +0800

    :tada: Initial commit

--- index.js
+++ index.js
@@ -7,4 +7,15 @@
  * @see https://github.com/nuintun/wxwork#readme
  */
 
-module.exports = 'Unrealized';
+'use strict';
+
+/**
+ * @module index
+ * @author nuintun
+ * @license MIT
+ * @version 2018/04/23
+ */
+
+const index = 'Unrealized';
+
+module.exports = index;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * @module index
+ * @author nuintun
+ * @license MIT
+ * @version 2018/04/23
+ */
+
+export default 'Unrealized';
